42f6ac2a51a759f30ddf8792fca7c6d099eb130b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-11 21:35:48 -0500

    [sm] did the first lesson on varialbes and contants from the 3rd section of the  py4e course

---
+++ 001_py4e/003_basic_python/variables_01.md
@@ -0,0 +1,11 @@
+# Building Blocks of Python
+
+## Constants
+Values that don't change
+
+## Variable
+- a pointer to where data is stored
+- use descriptive names
+- be careful with case
+- Don't start with a number
+- Don't start with an underscore for no reason, sometimes there will be a reason
---
+++ 001_py4e/003_basic_python/variables_01.py
@@ -0,0 +1,37 @@
+123
+print(123)
+12.3
+print(12.3)
+"this is a constant"
+print("this is a constant")
+
+x = 12.2
+y = 14
+print(f"x: {x}, y: {y}")
+
+x = 55
+y = 66
+print(f"x: {x}, y: {y}")
+
+# bad practice
+fd903rj3 = 12
+ds93j29j = 24
+blorpd9d92j = fd903rj3 + ds93j29j
+print(blorpd9d92j)
+
+# better practice
+number_one = 35.0
+number_two = 45.0
+multiplied = number_one * number_two
+print(multiplied)
+
+# even better practice
+hours = 40
+rate = 40
+pay = hours * rate
+print("pay:", pay)
+
+# evaluates from right to left so it has accesss to the last value
+cheese = 2
+cheese = 3 * cheese - cheese
+print("cheese:", cheese)
