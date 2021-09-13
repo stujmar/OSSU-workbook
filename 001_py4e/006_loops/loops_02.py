c29c31ac3ffbc15acb6885b5408f23bb7a53641b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-12 23:31:26 -0500

    [sm] finished second video on loop theory mostly in keyword and logic of loops

---
+++ 001_py4e/006_loops/loops_02.py
@@ -0,0 +1,19 @@
+# Definite loops or "for loops"
+
+def count_to(n):
+    for number in range(1, n):
+        print(number)   # Prints 1 to 10
+
+for i  in [5, 4, 3, 2, 1]:
+    print(i)        # Prints 5, 4, 3, 2, 1  
+print('Blastoff!')
+
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends:
+    print('Happy New Year:', friend)
+print('Who Ray!')
+
+# Chuck likes the word "in"
+# | such that
+# for each of the values "in" the list friends
+
