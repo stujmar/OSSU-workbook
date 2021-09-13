95f652d68ebf5830f7408ff2560cb6305cec5072
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-12 22:36:58 -0500

    [sm] did initial python functions video in py4e

---
+++ 001_py4e/005_functions/functions_01.py
@@ -0,0 +1,9 @@
+# DRY Don't reapeat yourself.
+# Keywords: functions, arguments, parameters, return, return values
+# def = define function
+
+def say_hello(name):
+    print("Hello " + name)
+
+say_hello("John")
+
