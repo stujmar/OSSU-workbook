617c8d9c680e5f2d0bf2ec5200e69156a6fffe65
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-15 22:16:25 -0500

    [sm] brought in reference file to read for exercise trouble shot root issue

---
+++ 001_py4e/008_files/ex_07_01.py
@@ -0,0 +1,10 @@
+# Write a program to read throught a file and pirnt the contents of the file line by line all in upper case.
+# Wrap the file read in a try block and print the contents of the file
+# except IOError as e:
+
+import os
+
+print(os.listdir("."))
+
+
+my_file = open('../008_files/my_file.txt')
