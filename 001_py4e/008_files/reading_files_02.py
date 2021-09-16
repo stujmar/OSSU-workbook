fc84472b3d6c8e2d46a60174f3cd3d2f4fc6ba0b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-15 22:08:54 -0500

    [sm] was able to scanner a letter and find the sender's name, pretty cool

--- 001_py4e/008_files/reading_files_02.py
+++ 001_py4e/008_files/reading_files_02.py
@@ -10,3 +10,9 @@ for cheese in xfile:
 
 yfile = open('../008_files/single_line.txt')
 print(yfile.readline())
+
+our_letter = open('../008_files/letter.txt')
+for line in our_letter:
+    if line.startswith('From:'):
+        start = line.find(':') + 1
+        print("the sender might be named:", line[start:])
