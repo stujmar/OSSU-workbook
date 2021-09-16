7e021b0a73fcf0034f529d1b279b5738abdf8d12
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-15 21:55:53 -0500

    [sm] did first video on reading files

---
+++ 001_py4e/008_files/reading_files_01.py
@@ -0,0 +1,13 @@
+# Reading from files
+
+# call the open() function to open a file.
+
+handle = open("filename", 'r')
+print(handle)
+
+# write a file that says "Hello World!"
+# file = open("filename.txt", 'w')
+# file.write("Hello World!")
+# file.close()
+
+# /n is a new line character
