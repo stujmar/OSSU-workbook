fb3d08a1656054eaa7f0aab15b27fa322f1b4f79
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-16 22:43:58 -0500

    [sm] cleaned up all old relative paths with new fix running in correct root

--- 001_py4e/008_files/reading_files_02.py
+++ 001_py4e/008_files/reading_files_02.py
@@ -3,15 +3,15 @@ import os
 print(os.listdir("."))
 
 # I have no idea why the root is in a different directory
-xfile = open('../008_files/test.txt')
+xfile = open('test.txt')
 for cheese in xfile:
     print(cheese)
 
 
-yfile = open('../008_files/single_line.txt')
+yfile = open('single_line.txt')
 print(yfile.readline())
 
-our_letter = open('../008_files/letter.txt')
+our_letter = open('letter.txt')
 for line in our_letter:
     if line.startswith('From:'):
         start = line.find(':') + 1
