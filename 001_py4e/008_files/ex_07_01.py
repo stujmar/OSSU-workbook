d8d03b8b28dccb663099badfd9aeb7b23c1aee67
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-18 08:21:37 -0500

    [sm] made a function to open the file with some i/o try catch

--- 001_py4e/008_files/ex_07_01.py
+++ 001_py4e/008_files/ex_07_01.py
@@ -8,17 +8,26 @@ print(os.listdir("."))
 
 collected_lines = []
 my_file = open('my_file.txt')
-print(my_file.read())
+# print(my_file.read())
 
 mbox = open('mbox-100.txt', 'r')
 print(mbox)
 
 # Get the lines from the file
-for line in mbox:
-    if line.startswith('X-DSPAM'):
-        line = line.rstrip()
-        collected_lines.append(line)
-print(collected_lines)
+# for line in mbox:
+#     if line.startswith('X-DSPAM'):
+#         line = line.rstrip()
+#         collected_lines.append(line)
+# print(collected_lines)
+
+# Open the file
+def open_file(file_name):
+    try:
+        file = open(file_name)
+        return file
+    except IOError as e:
+        print("Error: File not found", e)
+        return None
 
 # Function to check and return lines.
 def check_file(str, file, collection):
