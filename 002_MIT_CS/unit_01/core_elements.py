ecfa1d8fd84f28d219a522ccd795d019027b061a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-21 14:55:47 -0500

    [sm] did exercise on IDE from MIT course and took notes

---
+++ 002_MIT_CS/unit_01/core_elements.py
@@ -0,0 +1,13 @@
+# Editor to type in python expressions
+# IDE Integrated Development Environment
+# Text editors, shell, debug tools.
+
+x = 5
+
+if x != 5:
+    print("x is not 5")
+else:
+    print("x is 5")
+
+# ide can run entire scripts or selectively run certain scripts 
+# apparently
