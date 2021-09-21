b24d4f1f257f220802749ba3d4ec469dfc41d78d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-20 20:31:31 -0500

    [sm] did MIT course exercise 3_5_1 on digging into tuples

---
+++ 002_MIT_CS/unit_03/ex_03_05_01.py
@@ -0,0 +1,25 @@
+# Exercise 1
+
+x = (1, 2, (3, 'John', 4), 'Hi')
+
+print(x[0], 1, type(x[0]))
+
+print(x[2], (3, 'John', 4), type(x[2]))
+
+print(x[-1], 'Hi', type(x[-1]))
+
+print(x[2][2], 4, type(x[2][2]))
+
+print(x[-1][-1])
+
+# print(x[-1][2])
+
+print(2 in x)
+
+
+print(3 in x)
+# False
+
+print(x[0:1])
+
+# print(x[0] = 8)
