974f9b1bb567439b1ab0d995d5a59e4bd768d698
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-20 21:13:52 -0500

    [sm] finished first problem set for lists

---
+++ 002_MIT_CS/unit_03/ex_03_05_ex2.py
@@ -0,0 +1,19 @@
+# Exercise 2
+# Lists
+
+x = [1, 2, [3, 'John', 4], 'Hi']
+
+print(x[0], 1, type(x[0]))
+
+
+print(x[2], [3, 'John', 4], type(x[2]))
+
+print(x[-1], 'Hi', type(x[-1]))
+
+print(x[2][2], 4, type(x[2][2]))
+
+print(x[0:1], 1, type(x[0:1]))
+
+print(2 in x, True, type(2 in x))
+
+print(3 in x, False, type(3 in x))
