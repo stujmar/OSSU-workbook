8a5f634e0b2c6ab338d65320f82c2a373ff41e6f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-23 21:47:36 -0500

    [sm] did the second exercise in 01 02 05 of the MIT CS course

--- 002_MIT_CS/unit_01/section_two/ex_01_02_05.py
+++ 002_MIT_CS/unit_01/section_two/ex_01_02_05.py
@@ -5,4 +5,12 @@ def question_one():
     print(num)
 
 # 0, 1, 2, 3, 4, 4,
-question_one()
+# question_one()
+
+def question_two():
+    divisor = 2
+    for num in range(0, 10, 2):
+        print(num/divisor)
+
+#  0.0, 1.0, 2.0, 3.0, 4.0
+question_two()
