c9734e6b2d6e1e5f234f9641e2bed68ba632ecff
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-23 21:50:36 -0500

    [sm] did the third question in 01_02_05 exercise in the MIT CS course

--- 002_MIT_CS/unit_01/section_two/ex_01_02_05.py
+++ 002_MIT_CS/unit_01/section_two/ex_01_02_05.py
@@ -13,4 +13,14 @@ def question_two():
         print(num/divisor)
 
 #  0.0, 1.0, 2.0, 3.0, 4.0
-question_two()
+# question_two()
+
+def question_three():
+    for variable in range(20):
+        if variable % 4 == 0:
+            print(variable)
+        if variable % 16 == 0:
+            print('Foo!')
+
+# 0, Foo!, 4, 8, 16, Foo!
+question_three()
