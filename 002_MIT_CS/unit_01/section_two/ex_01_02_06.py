a9b0e30bd1d6e81730c1ad530cf3318b88e9173b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-24 20:03:36 -0500

    [sm] did the second problem in the 1.2.6 problem set in the MIT CS course

--- 002_MIT_CS/unit_01/section_two/ex_01_02_06.py
+++ 002_MIT_CS/unit_01/section_two/ex_01_02_06.py
@@ -6,4 +6,16 @@ def question_one():
         print(char)
     print('done')
 
-question_one()
+# question_one()
+
+def question_two():
+    greeting = 'Hello!'
+    count = 0
+    for letter in greeting:
+        count += 1
+        if count % 2 == 0:
+            print(letter)
+        print(letter)
+    print('done')
+
+# question_two()
