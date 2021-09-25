675de781ffee2f95328e3f0e7c0e6ebb35345b15
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-24 20:53:47 -0500

    [sm] did question 1 on exercise 2.3.1 for MIT CS course

---
+++ 002_MIT_CS/unit_02/ex_02_03_01.py
@@ -0,0 +1,15 @@
+# Exercise 2.3.1
+
+def question_one():
+    iteration = 0
+    count = 0
+    while iteration < 5:
+        # the variable 'letter' in the loop stands for every 
+        # character, including spaces and commas!
+        for letter in "hello, world": 
+            count += 1
+        print("Iteration " + str(iteration) + "; count is: " + str(count))
+        iteration += 1 
+
+# question_one()
+
