249a7c174ffb4ffdbef52ebbd1e1e3ee8ba4a1d4
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-25 11:23:06 -0500

    [sm] made a method to check for a cubed root using a bisectional search

--- 002_MIT_CS/unit_02/02_03_bisection.py
+++ 002_MIT_CS/unit_02/02_03_bisection.py
@@ -1,6 +1,6 @@
 # Bisection Search
 
-def find_squareroot(x):
+def find_square_root(x):
     epsilon = 0.01
     numGuesses = 0
     low = 1.0
@@ -18,4 +18,23 @@ def find_squareroot(x):
     print('numGuesses = ' + str(numGuesses))
     print(str(ans) + ' is close to square root of ' + str(x))
 
-# find_squareroot(100)
+# find_square_root(100)
+
+def find_cubed_root(x):
+    epsilon = 0.01
+    numGuesses = 0
+    low = 1.0
+    high = x
+    ans = (high + low)/2.0
+    while abs(ans**3 - x) >= epsilon:
+        print('low =', low, 'high =', high, 'ans =', ans)
+        numGuesses += 1
+        if ans**3 < x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low)/2.0
+    print('numGuesses = ' + str(numGuesses))
+    print(str(ans) + ' is close to cube root of ' + str(x))
+
+find_cubed_root(27)
