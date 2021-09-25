e0ccee8dca148945ca9724a98b510ca7de4b493e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-24 20:49:30 -0500

    [sm] did so far video and went over loops while for strings indexing and all the stuff we had learned so far

---
+++ 002_MIT_CS/unit_02/02_03_so_far.py
@@ -0,0 +1,28 @@
+# What have we learned so far.
+
+# For loops
+# While loops
+# Functions
+# Iterations
+
+# Now using strings to generate algorithms
+
+# String are case sensitive
+# Strings can be compared.
+
+if "this" != "that":
+    print("this is not that")
+
+# Strings are ordered with indices
+data = "<ooXoo>"
+print(data[3], "marks the spot.")
+
+# Strings are immutable
+
+
+# range is a nice way to loop of numbers.
+# for can also loop over strings
+
+for index in range(len(data)):
+    print(index, data[index])
+# Cool.
