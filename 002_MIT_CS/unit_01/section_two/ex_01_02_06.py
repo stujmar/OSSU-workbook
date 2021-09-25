a1fc865dd858c33f0a83e50af78cd0bb0d9c5947
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-24 20:07:48 -0500

    [sm] did problems for exercise 1.2.6 in the MIT CS course

--- 002_MIT_CS/unit_01/section_two/ex_01_02_06.py
+++ 002_MIT_CS/unit_01/section_two/ex_01_02_06.py
@@ -19,3 +19,20 @@ def question_two():
     print('done')
 
 # question_two()
+
+def question_three():
+    school = 'Massachusetts Institute of Technology'
+    numVowels = 0
+    numCons = 0
+    
+    for char in school:
+        if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+            numVowels += 1
+        elif char == 'o' or char == 'M':
+            print(char)
+        else:
+            numCons -= 1
+    print('numVowels is: ' + str(numVowels))
+    print('numCons is: ' + str(numCons))
+
+# question_three()
