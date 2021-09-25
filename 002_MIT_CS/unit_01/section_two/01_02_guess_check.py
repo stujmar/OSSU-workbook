b8565fa835d65a4bf398cdc592aa3600f3924b1b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-24 20:17:42 -0500

    [sm] did cool method to check cubed roots via the guess and check method

---
+++ 002_MIT_CS/unit_01/section_two/01_02_guess_check.py
@@ -0,0 +1,23 @@
+# Iterative algorithm to guess a number
+# using loops
+
+# Guess and check methods
+
+def run_my_loop():
+    x = int(input("Enter a number: "))
+
+    def check_number(x):
+        ans = 0
+
+        while ans**3 < x:
+            ans = ans + 1
+        if ans**3 == x:
+            # print(str(x) + " is not a perfect cube")
+            print("Cube root of " + str(x) + " is " + str(ans))
+        # else:
+
+    for num in range(0, x + 1):
+        check_number(num)
+
+# run_my_loop()
+
