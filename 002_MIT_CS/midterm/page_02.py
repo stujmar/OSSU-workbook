55542c37e1acde4b657b8477105aeea3556a0c45
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-26 19:44:24 -0500

    [sm] did the second page of the CS MIT midterm

---
+++ 002_MIT_CS/midterm/page_02.py
@@ -0,0 +1,24 @@
+# Problem 2-1
+
+# L mapes stings to  to integers
+L = {'1':1, '2':2, '3':3}
+
+L = [1,2,3]
+d = {'a': 'b'}
+def f(x):
+    return 3
+
+# print(L[3]) IndexError: list index out of range
+# print(d['b']) KeyError: 'b'
+
+# for i in range(100001, -1, -2):
+#     print(f)
+
+# Problem 2 - 6
+stuff = ["iQ"]
+for thing in stuff:
+    print(thing)
+
+# Problem 2 - 7
+# is Square a reserved word?
+
