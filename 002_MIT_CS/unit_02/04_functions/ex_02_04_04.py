d06b4263e1db50c362baba0f013991b090efaeab
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-27 12:29:35 -0500

    [sm] did second problem set in MIT CS course exercise 2.4.4

--- 002_MIT_CS/unit_02/04_functions/ex_02_04_04.py
+++ 002_MIT_CS/unit_02/04_functions/ex_02_04_04.py
@@ -8,3 +8,16 @@ a = 3
 # estimated output: int 4 
 print(f(1))
 # actual output: int 4 
+
+# Problem 2.4.4.2
+
+x = 12
+def g(x):
+    x = x + 1
+    def h(y):
+        return x + y
+    return h(6)
+
+# estimated output: int 19
+print(g(x))
+# actual output: int 19
