6523091abc70f503f4e874e3f39d8b04fc7f9cc2
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-26 20:48:35 -0500

    [sm] did binding video and took notes on MIT CS course

---
+++ 002_MIT_CS/unit_01/vd_01_02_bindings.py
@@ -0,0 +1,26 @@
+# Bindings
+
+# Variables
+# Often called bindings
+
+# Python has a special feature called a binding.
+# A binding is a connection between a variable and a value.
+x = 1
+
+# Python strings are ordered and indexed
+y = "two"
+
+# Python floats are not integers
+z = 3.14
+
+# Python lists are mutable
+my_list = [1, 2, 3]
+
+# Python dictionaries are like hashes in Perl
+my_dict = {'one': 1, 'two': 2, 'three': 3}
+
+# Python tuples are immutable
+my_tuple = (1, 2, 3)
+
+# Python sets are unordered collections of unique elements
+my_set = {1, 2, 3}
