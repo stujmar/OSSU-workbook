a10a27723812475f64dc5ca08313ce517bafc2ce
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-28 20:14:09 -0500

    [sm] coded along with the Specifications video on MIT CS course

---
+++ 002_MIT_CS/unit_02/04_functions/02_04_specification.py
@@ -0,0 +1,13 @@
+# Function Specification
+# What we assume to be true about parameters
+# - Contract
+# - Assumptions
+# - Guarantees
+
+def is_even(x):
+    """
+    Returns True if x is even, False otherwise.
+    """
+    return x % 2 == 0
+
+# Good programmers use docstrings/annotations to document their functions.
