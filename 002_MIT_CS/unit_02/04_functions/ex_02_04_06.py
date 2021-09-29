0e339e1d4d5742c9a80ee7c70089af5121665f83
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-28 20:45:08 -0500

    [sm] did exercise 2.4.6 on MIT CS course

---
+++ 002_MIT_CS/unit_02/04_functions/ex_02_04_06.py
@@ -0,0 +1,54 @@
+# Exercise 2.4.6
+
+str1 = 'exterminate!'
+str2 = 'number one - the larch'
+
+# 1
+print(str1.upper)
+
+# 2
+print(str1.upper())
+
+# 3
+print(str1)
+
+# 4
+print(str1.isupper())
+
+# 5
+print(str1.islower())
+
+# 6
+str2 = str2.capitalize()
+print(str2)
+
+# 7 
+print(str2.swapcase())
+
+# 8
+print(str1.index('e'))
+
+# 9
+print(str2.index('n'))
+
+# 10
+print(str2.find('n'))
+
+# 11
+print('11')
+print(str2.index('!'))
+
+# 12
+print("12")
+print(str2.find('!'))
+
+# 13
+print('13')
+print(str1.count('e'))
+
+# 14
+str1 = str1.replace('e', '*')
+print(str1)
+
+print(str2.replace('one', 'seven'))
+
