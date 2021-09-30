bdf73b8bc92ef8e55378ac2b5aab29aae877008e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-29 21:17:09 -0500

    [sm] created a program to write text into an external file

--- 002_MIT_CS/unit_02/04_functions/vd_files.py
+++ 002_MIT_CS/unit_02/04_functions/vd_files.py
@@ -14,3 +14,13 @@ print(area(3))
 
 # print(circle.circumference(3))
 print(circumference(3))
+
+nameHandle = open('kids', 'w')
+for i in range(2):
+    name = input("Enter name: ")
+    nameHandle.write(name + '\n')
+nameHandle.close()
+
+# nameHandle = open('kids', 'r')
+# for line in nameHandle:
+#     print(line, end='')
