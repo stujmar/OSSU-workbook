f1ad0bd21fc301b6076cfe144bcce5633181550b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-30 22:52:20 -0500

    [sm] debugged a recurive modulo function for MIT CS course

---
+++ 002_MIT_CS/unit_04/ex_04_07_06.py
@@ -0,0 +1,16 @@
+def rem(x, a):
+    """
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the remainder when x is divided by a.
+    """
+    if x == a:
+        return 0
+    elif x < a:
+        return x
+    else:
+        # changed the following line.
+        return rem(x-a, a)
+
+print(rem(10, 5))
