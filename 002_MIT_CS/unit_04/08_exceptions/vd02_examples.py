fa7d3dec480302ab591fb9dab0b59b856a6f6305
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-01 15:54:10 -0500

    [sm] working through examples of using exceptions in different scenarios

---
+++ 002_MIT_CS/unit_04/08_exceptions/vd02_examples.py
@@ -0,0 +1,47 @@
+# Example of Exception Usage
+
+def main():
+    try:
+        print("Enter a number: ")
+        num = int(input())
+        print("You entered: ", num)
+    except ValueError:
+        print("That's not a number!")
+    except:
+        print("Something went wrong!")
+    else:
+        print("No exceptions were raised!")
+    # This lets us always do something like cleanup even if there is an exception.
+    finally:
+        print("This is always printed!")
+
+def clean_input():
+    while True:
+        try:
+            n = int(input("Enter an integer: "))
+            n = int(n)
+            break
+        except ValueError:
+            print("Oops! That was no valid number. Try again...")
+    print("Correct input of an integer")
+
+def gradeData(data):
+    for student in data:
+        try:
+            grade = int(student[-1])
+        except ValueError:
+            print("Error: Invalid grade")
+            continue
+        if grade < 0 or grade > 100:
+            print("Error: Invalid grade")
+            continue
+        if grade < 60:
+            print("Warning: Grade is less than 60")
+        elif grade < 70:
+            print("Warning: Grade is less than 70")
+        elif grade < 80:
+            print("Warning: Grade is less than 80")
+        elif grade < 90:
+            print("Warning: Grade is less than 90")
+        else:
+            print("Grade is good")
