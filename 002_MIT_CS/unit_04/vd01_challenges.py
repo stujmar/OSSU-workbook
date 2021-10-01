71f2ac53d8fdfc61318604bbbe4d95c71daa6d3c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-30 20:58:05 -0500

    [sm] watched video about debugging and testing and worked along with it.

---
+++ 002_MIT_CS/unit_04/vd01_challenges.py
@@ -0,0 +1,15 @@
+# Debugging:
+#   1. Run the program and see if it works.
+#   2. If it doesn't, try to figure out what went wrong.
+#   3. If you can't figure it out, ask for help.
+#   4. If you can't figure it out, ask for help.
+#   5. If you can't figure it out, ask for help.
+
+# There is bugs in your soup.
+# You need to fix it: Defensive programming.
+# Check the soup for bugs: Testing.
+# Keep the lid closed: Defensive programming.
+# Clean the kitchen: Debug your code.
+
+# Write modular programming.
+# Write unit testing.
