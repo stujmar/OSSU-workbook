d921510b9ecd87761c573173d91a72c11afcf9d1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-30 22:28:03 -0500

    [sm] did video and took notes on overt/covert bugs and persistent/transient bugs

---
+++ 002_MIT_CS/unit_04/vd03_bugs.py
@@ -0,0 +1,23 @@
+# Once you find your code doesn't work
+# Isolate the bugs.
+# Fix them
+# Retest
+
+# The first bug was a literal bug (a motherfucking literal bug) that was
+# causing the program to crash. A moth. Grace Hopper.
+# The second bug was a logic bug that was causing the program to crash.
+# The third bug was a logic bug that was causing the program to crash.
+# The fourth bug was a logic bug that was causing the program to crash.
+# The fifth bug was a logic bug that was causing the program to crash.
+# The sixth bug was a logic bug that was causing the program to crash.
+# The seventh bug was a logic bug that was causing the program to crash.
+# The eighth bug was a logic bug that was causing the program to crash.
+
+# Where do digital computer bugs come from.
+# There are overt bugs and covert bugs.
+# Overt bugs are bugs that are visible to the user.
+# Covert bugs are bugs that are not visible to the user.
+
+# There are overt/persistent bugs and transient/covert (or intermittent) bugs.
+# Persistent bugs are much easier to fix than transient bugs.
+
