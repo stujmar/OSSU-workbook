63926871280b1db8d57c0a65b5ca8492ce1a92ae
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-30 22:19:28 -0500

    [sm] did more of the MIT CS exercises picking test cases for different functions

---
+++ 002_MIT_CS/unit_04/ex_04_07_05.py
@@ -0,0 +1,16 @@
+# Exercise 4.7.5
+
+# Consider the following function definition:
+
+def foo(x, a):
+   """
+   x: a positive integer argument
+   a: a positive integer argument
+
+   returns an integer
+   """
+   count = 0
+   while x >= a:
+      count += 1
+      x = x - a
+   return count
