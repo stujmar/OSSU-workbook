a38c36e249cb012746b70aa2c853dca309d71e1f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-01 15:14:21 -0500

    [sm] debugged palindrome program and helper functions on MIT CS course

--- 002_MIT_CS/unit_04/vd05_debugging_example.py
+++ 002_MIT_CS/unit_04/vd05_debugging_example.py
@@ -5,21 +5,35 @@
 
 def isPal(x):
     assert type(x) == list
-    temp = x
+    temp = x[:]
+    # This line was changed from temp.reverse()
     temp.reverse()
     if temp == x:
         return True
     else:
         return False
 
+# def silly(n):
+#     for i in range(n):
+#         result = []
+#         elem = input('Enter element: ')
+#         result.append(elem)
+#     if isPal(result):
+#         print('Yes, it is a palindrome')
+#     else:
+#         print('No, it is not a palindrome')
+
 def silly(n):
+    result = []
     for i in range(n):
-        result = []
         elem = input('Enter element: ')
         result.append(elem)
     if isPal(result):
         print('Yes, it is a palindrome')
     else:
         print('No, it is not a palindrome')
+
+
+silly(3)
 
 # This code likely has bugs.
