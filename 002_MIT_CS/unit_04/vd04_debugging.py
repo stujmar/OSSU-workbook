f5a745128872829b846f3ff6205089a79636acc7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-30 22:45:44 -0500

    [sm] watched video on debugging code and wrote notes along with it for MIT CS course

---
+++ 002_MIT_CS/unit_04/vd04_debugging.py
@@ -0,0 +1,45 @@
+# Debugging has a steep learning curve.
+
+# Be patient.
+# There are tools to help you debug.
+# They are called debugging tools.
+
+# also use lots of print statements.
+
+
+# Look into python tutor.
+
+# Weird! 
+# https://www.youtube.com/watch?v=_X-_7_X-_X-
+
+# move print statement through the code to find where it jams up.
+
+# IndexError: list index out of range
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
+# ValueError: invalid literal for int() with base 10: '1'
+# ZeroDivisionError: division by zero
+# NameError: name 'x' is not defined
+# SyntaxError: invalid syntax (missing parenthesis)
+# AttributeError: 'int' object has no attribute 'y'
+# KeyError: 'key'
+# AssertionError: assert False
+# RuntimeError: maximum recursion depth exceeded
+# RecursionError: maximum recursion depth exceeded
+# EOFError: unexpected EOF while parsing
+# ImportError: No module named 'x'
+# IndentationError: unexpected indent
+# TabError: unexpected indent with tabs
+# SystemError: (IOError) [Errno 2] No such file or directory: 'x'
+
+# Logic errors are harder. Thinking and planning is key. Draw it out on paper.
+# Think about the problem.
+# Think about the steps.
+# Think about the results.
+# Explain the code. Say it out loud. Explain it to the duck.
+# Explain the code to the duck.
+# Explain the code to the human.
+# Take a break.
+
+# Write modules at a time test and repeat.
+# Then write integration tests.
+
