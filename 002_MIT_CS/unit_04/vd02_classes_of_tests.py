f801cffcb4c31248bc21c6ba5caf051f8f37a448
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-30 21:10:39 -0500

    [sm] watched video and took notes on different types of testing and philosophy of testing

---
+++ 002_MIT_CS/unit_04/vd02_classes_of_tests.py
@@ -0,0 +1,35 @@
+# Video: Classes of tests
+
+# Once you have written your code, you can run the tests.
+# Break the code into simple testable modules.
+
+# Good Docstrings are a great helper for testing.
+# Docstrings are used to document your code.
+
+# Motherhood and apple pie approach to testing.
+# The idea is to test the code as you write it.
+# It is a universal truth.
+
+# 1. Ensure that your code is working as expected. Remove errors and print statements.
+
+# 2. Write Unit Tests. and retest. 
+
+# 3. Regression testing. Reintroduce bugs?
+
+# 4. Integration testing. Test how the pieces fit together. Don't run to this step.
+
+# Use your intuition to write tests.
+#-------
+
+# Black box testing.
+# explores all the paths in and out of functions.
+
+# Glass box testing.
+# explores the code itself.
+
+# Test the extremes.
+# Test the boundaries.
+
+
+
+
