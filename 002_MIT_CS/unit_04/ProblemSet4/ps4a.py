7755b042c0040e683e8accf21a6facee9bcae46f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-03 12:29:31 -0500

    [sm] implemented playhand for problem set 4 in MIT CS course

--- 002_MIT_CS/unit_04/ProblemSet4/ps4a.py
+++ 002_MIT_CS/unit_04/ProblemSet4/ps4a.py
@@ -225,34 +225,36 @@ def playHand(hand, wordList, n):
       n: integer (HAND_SIZE; i.e., hand size required for additional points)
       
     """
-    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
-    # Keep track of the total score
-    
+    score = 0
     # As long as there are still letters left in the hand:
-    
+    while calculateHandlen(hand) > 0:
         # Display the hand
-        
+        print('Current Hand:', end=' '); displayHand(hand)     
         # Ask user for input
-        
+        guess = str(input('Enter word, or a "." to indicate that you are finished: '))        
         # If the input is a single period:
-        
+        if guess == '.':        
             # End the game (break out of the loop)
-
-            
+            break            
         # Otherwise (the input is not a single period):
-        
+        else:        
             # If the word is not valid:
-            
+            if isValidWord(guess, hand, wordList) == False:            
                 # Reject invalid word (print a message followed by a blank line)
-
+                print('Invalid word, please try again.', '\n')
             # Otherwise (the word is valid):
-
+            else:
                 # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
-                
+                score += getWordScore(guess, n)
+                print('"'+guess+'"', "earned", getWordScore(guess, n), "points. Total:", score, "points", '\n')
                 # Update the hand 
-                
+                hand = updateHand(hand, guess)               
 
     # Game is over (user entered a '.' or ran out of letters), so tell user the total score
+    if guess == '.':
+        print('Goodbye! Total score:', score, 'points.')
+    else:
+        print('Run out of letters. Total score:', score, 'points.')
 
 
 #
