5011c3c17c7d1428b996fbde97ceee9ebc7191a2
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-02 19:59:59 -0500

    [sm] did the simple divide exercise in the raising exceptions section of the MIT CS course

---
+++ 002_MIT_CS/unit_04/08_exceptions/ex_04_08_simple_divide.py
@@ -0,0 +1,11 @@
+def fancy_divide(list_of_numbers, index):
+   denom = list_of_numbers[index]
+   return [simple_divide(item, denom) for item in list_of_numbers]
+
+
+def simple_divide(item, denom):
+    try:
+        return item / denom
+    except ZeroDivisionError:
+        return 0
+        # return [0,0,0] doesn't work because it returns a list of lists
