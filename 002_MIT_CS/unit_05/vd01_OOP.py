87be1744e4b57f4e08923d4c26af53756c5e0d55
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-04 19:57:05 -0500

    [sm] watched video on OOP and coded along, MIT CS course

---
+++ 002_MIT_CS/unit_05/vd01_OOP.py
@@ -0,0 +1,31 @@
+# Object Oriented Programming
+
+# Object has a type, state, and behavior
+# Object is a type
+# Object has a state internal data representation
+# Object has a behavior
+
+# Instances. 1 3 5 6 are instances of the class int
+# "hello" is an instance of the class str
+
+# Data abstraction is hiding the implementation details of the object
+# you can delete objects either by del or by garbage collection
+
+# Lists Tuples and Strings are built in objects.
+# We'll look at those and make our own.
+
+[1, 2, 3] # instance of list
+
+# Linked lists each points to the next element or cell in memory.
+# internal proceedures of an object should be private and only defined interfaces should be used.
+
+# How do we make our own objects?
+# We'll start with a class.
+
+L = [1, 2, 3]
+
+print(len(L))
+
+# Bundle data into packages.
+
+# inheritance is a way to share code between classes, via the use of a base class. and the derived class inherits all the methods and attributes of the base class.
