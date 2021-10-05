7631badd831350bad65ef7ab38c6e1a949fae96d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-04 20:28:46 -0500

    [sm] did ex 5.9.1 on classes and objects in MIT CS course

---
+++ 002_MIT_CS/unit_05/ex_05_09_01.py
@@ -0,0 +1,11 @@
+# Question One
+# the __init__ method is a special method that is called when an object is created from a class.
+
+# Question Two
+# To call the doSomething method on an instance called obj you would write obj.doSomething()
+
+# Question Three
+# An objects attributes are defined in the __init__ method. False, you can defince things outside the __init__ method apparently.
+
+# Question Four
+# To pass a paramter to the __init__ method you would write self.parameter = parameter
