4922dc3261f3a5e567d718d1fb8f645a3aa2dbf7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-06 21:56:54 -0500

    [sm] built class to interact with a set of integers

--- 002_MIT_CS/unit_05/vd04_classes.py
+++ 002_MIT_CS/unit_05/vd04_classes.py
@@ -32,3 +32,24 @@ print(my_half.get_denominator())
 my_third = fraction(1, 3)
 
 print(my_half + my_third)
+
+class intSet(object):
+    def __init__(self):
+        self.vals = []
+    def insert(self, e):
+        if not e in self.vals:
+            self.vals.append(e)
+    def member(self, e):
+        return e in self.vals
+    def remove(self, e):
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+    def __str__(self):
+        self.vals.sort()
+        result = ''
+        for e in self.vals:
+            result = result + str(e) + ','
+        return '{' + result[:-1] + '}'
+
