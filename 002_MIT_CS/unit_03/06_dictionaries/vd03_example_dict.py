f1be346c0368548c81eb9507526300520668a206
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-09 20:09:45 -0500

    [sm] worked half way through word frequency problem and got messed up so starting to back track

---
+++ 002_MIT_CS/unit_03/06_dictionaries/vd03_example_dict.py
@@ -0,0 +1,74 @@
+# Example of creating and using a dictionary.
+
+she_loves_you = ['she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+
+'you', 'think', "you've", 'lost', 'your', 'love',
+'well', 'i', 'saw', 'her', 'yesterday-yi-yay',
+"it's", 'you', "she's", 'thinking', 'of',
+'and', 'she', 'told', 'me', 'what', 'to', 'say-yi-yay',
+
+'she', 'says', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'she', 'said', 'you', 'hurt', 'her', 'so',
+'she', 'almost', 'lost', 'her', 'mind',
+'and', 'now', 'she', 'says', 'she', 'knows',
+"you're", 'not', 'the', 'hurting', 'kind',
+
+'she', 'says', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+
+'you', 'know', "it's", 'up', 'to', 'you',
+'i', 'think', "it's", 'only', 'fair',
+'pride', 'can', 'hurt', 'you', 'too',
+'pologize', 'to', 'her',
+
+'Because', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'Yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'yeah', 'yeah', 'yeah',
+'yeah', 'yeah', 'yeah', 'yeah'
+]
+
+def lyrics_to_frequencies(lyrics):
+    my_dict = {}
+    for word in lyrics:
+        if word.lower() in my_dict:
+            my_dict[word.lower()] += 1
+        else:
+            my_dict[word.lower()] = 1
+    return my_dict
+
+word_counts = lyrics_to_frequencies(she_loves_you)
+print(word_counts)
+
+def most_frequent(freqs, min_times):
+    results = []
+    done = False
+    while not done:
+        temp
+    pass
+
+def least_frequent():
+    pass
