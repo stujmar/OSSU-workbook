ae2df2d332f74d6816d106a73f4143d8fb227640
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-10 16:55:46 -0500

    [sm] did first page firt 4 problems of problem set 6 in the MIT CS course

---
+++ 002_MIT_CS/unit_06/PS_06/problem_01.py
@@ -0,0 +1,17 @@
+# Problem Set 6
+
+# Problem 1-1
+# The ONLY thing we are interested in when designing programs is that it returns the correct answer.
+# False >>> False!
+
+# Problem 1-2
+# When determining asymptotic complexity, we discard all terms except for the one with the largest growth rate.
+# True >>> True! You can ignore lower terms.
+
+# Problem 1-3
+# Bisection search is an example of linear time complexity.
+# True >>> False! Because it isn't exponential. Apparently it isn't.
+
+# Problem 1-4
+# For large values of n, an algorithm that takes 20000n^2 steps has better time complexity (takes less time) than one that takes 0.001n^5 steps
+# True
