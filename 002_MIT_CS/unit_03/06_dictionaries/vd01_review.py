83b265689054a559f1a7a3fcf4fae5921e9ed80f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-09 19:37:21 -0500

    [sm] did entry video for dictionaries section of MIT CS course and took notes

---
+++ 002_MIT_CS/unit_03/06_dictionaries/vd01_review.py
@@ -0,0 +1,11 @@
+# 3.6 Dictionaries Review
+
+# Ordered collections of elements
+# Dictionaries are unordered collections of key-value pairs
+# Keys must be unique
+# Keys are immutable
+# Values can be any type
+
+# Lists, Strings, Tuples, Ranges are all ordered collections
+# Dictionaries are unordered collections of key-value pairs
+
