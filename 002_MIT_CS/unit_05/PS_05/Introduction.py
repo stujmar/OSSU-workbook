60fbb63685f953a3ba310eca006a9c195b435775
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-11 20:02:50 -0500

    [sm] did introduction to problem set 5 and took notes, for MIT CS course

---
+++ 002_MIT_CS/unit_05/PS_05/Introduction.py
@@ -0,0 +1,17 @@
+# Problem Set 5 Introduction
+
+# Encryption is a very popular method of data hiding.
+# It is used to hide data in a file or message.
+
+# A cypher is an algorithm that encrypts a message.
+# The algorithm is called a cypher because it encrypts the message.
+
+# plaintext -> cyphertext
+
+# A cypher usually relies on an outside pieces of information, most often a key.
+
+# The Caesar cypher is a simple example of a cypher.
+# Shift each letter by a certain amount. Letter = letter + key.
+
+import string
+print( string.ascii_lowercase )
