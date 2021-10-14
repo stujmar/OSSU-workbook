934993e44a90473838542d39034116c46a52a146
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-13 21:23:19 -0500

    [sm] finished exercise 2.3.3 in the MIT CS course

---
+++ 002_MIT_CS/unit_02/03_algorithms/ex_02_03_03.py
@@ -0,0 +1,12 @@
+# Exercise 2.3.3
+
+# Question One
+# True or False? The internal computer representation of any number is always an approximation. >> False
+
+# Question Two
+# The decimal 11 is what binary? >> 11 = 1011
+
+# Question Three
+# True or False? The internal representation of the decimal number 1/10 = 0.1 requires an infinite number of digits. >> TRUE!?!?
+# Question Four
+# After many computations, you get two floating numbers stored in variables a and b. Your code compares the numbers with a == b.
