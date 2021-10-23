758e83a02df76e6caae20d14f41ca124058477aa
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-23 17:22:18 -0500

    [sm] did second and thrid apply to each methods in the MIT CS course

--- 002_MIT_CS/unit_03/ex_apply_to_each.py
+++ 002_MIT_CS/unit_03/ex_apply_to_each.py
@@ -8,3 +8,15 @@ testList = [1, -4, 8, -9]
 
 applyToEach(testList, abs)
 print(testList)
+
+# Exercise Apply to Each 02
+
+def add_one(x):
+    return x + 1
+applyToEach(testList, add_one)
+
+# Exercise Apply to Each 03
+
+def square(x):
+    return abs(x) * abs(x)
+applyToEach(testList, square)
