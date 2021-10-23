2824144ce3d9531dee2140e6464c2b2df2f19f62
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-22 20:59:46 -0500

    [sm] did page one of the exam for the MIT CS course

---
+++ 002_MIT_CS/exam/Page_01.py
@@ -0,0 +1,28 @@
+
+# Problem 1-1
+
+# In the statement L = [1,2,3], L is a class.
+# L is a list. L is a variable. False, it is not a class.
+
+# Problem 1-2
+
+# A term is a combination of variables and constants. or variables and coefficients.
+my_term = "2a"
+# a polynomial is a combination of terms.
+
+# Problem 1-3
+
+# True the complexity of a binary search on a sorted list is O(log n).
+
+# Problem 1-4
+
+# True A bisection search algorithm always returns the correct answer when searching for an element in a sorted list.
+
+# Problem 1-5
+
+# False Performing binary search on an unsorted list will always return the correct answer in O(n) time. 
+# On an unsorted list to get the correct answer, we need to sort the list, or we need to use a linear search algorithm.
+
+# Problem 1-6
+
+
